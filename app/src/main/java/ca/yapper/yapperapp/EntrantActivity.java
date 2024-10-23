6409ace5af06c9ec0375300d62466813c95be471
Nathan-Correia <ncorreia@ualberta.ca>
2024-10-23 00:51:32 -0600

    Entrant/Organizer NavBar

---
+++ app/src/main/java/ca/yapper/yapperapp/EntrantActivity.java
@@ -0,0 +1,62 @@
+package ca.yapper.yapperapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class EntrantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_entrant);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                if (item.getItemId() == R.id.nav_entrant_home) {
+                    selectedFragment = new EntrantHomeFragment();
+
+                } else if (item.getItemId() == R.id.nav_entrant_qrscanner) {
+                    selectedFragment = new EntrantQRCodeScannerFragment();
+
+                } else if (item.getItemId() == R.id.nav_entrant_notifications) {
+                    selectedFragment = new EntrantNotificationsFragment();
+
+                } else if (item.getItemId() == R.id.nav_entrant_profile) {
+                    selectedFragment = new ProfileFragment();
+                }
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                }
+                return true;
+            }
+        });
+
+        // Default fragment
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EntrantHomeFragment()).commit();
+        }
+    }
+}
