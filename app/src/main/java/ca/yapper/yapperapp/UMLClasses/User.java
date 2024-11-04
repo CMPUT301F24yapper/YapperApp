704af55cb0adca8334fc59cd29791335a2e9bc7e
Mohammedmostain <122962236+Mohammedmostain@users.noreply.github.com>
2024-11-04 11:06:56 -0700

    fixed up some thing User.java and added isOptedOut

--- app/src/main/java/ca/yapper/yapperapp/UMLClasses/User.java
+++ app/src/main/java/ca/yapper/yapperapp/UMLClasses/User.java
@@ -14,11 +14,12 @@ public class User {
     private Boolean isEntrant;
     private Boolean isOrganizer;
     private Boolean isAdmin;
+    private Boolean isOptedOut; // Added attribute for notification opt-out status
 
     // Default constructor (required for Firestore deserialization)
     public User() {}
     // constructor version with parameters
-    public User(String name, String deviceId, String email, String phoneNum, Boolean isEntrant, Boolean isOrganizer, Boolean isAdmin) {
+    public User(String name, String deviceId, String email, String phoneNum, Boolean isEntrant, Boolean isOrganizer, Boolean isAdmin, Boolean isOptedOut) {
         this.deviceId = deviceId;
         this.name = name;
         this.email = email;
@@ -28,6 +29,7 @@ public class User {
         this.isEntrant = isEntrant;
         this.isOrganizer = isOrganizer;
         this.isAdmin = isAdmin;
+        this.isOptedOut = isOptedOut; // Initialize in the constructor
     }
 
     public String getName() {
@@ -39,7 +41,6 @@ public class User {
     }
 
     public String getDeviceId() {
-
         return deviceId;
     }
 
@@ -64,20 +65,20 @@ public class User {
     }
 
     /** public ProfilePic getProfilePic() {
-        return profilePic;
-    }
+     return profilePic;
+     }
 
-    public void setProfilePic(ProfilePic profilePic) {
-        this.profilePic = profilePic;
-    }
+     public void setProfilePic(ProfilePic profilePic) {
+     this.profilePic = profilePic;
+     }
 
-    public ProfilePic getGeneratedProfilePic() {
-        return generatedProfilePic;
-    }
+     public ProfilePic getGeneratedProfilePic() {
+     return generatedProfilePic;
+     }
 
-    public void setGeneratedProfilePic(ProfilePic generatedProfilePic) {
-        this.generatedProfilePic = generatedProfilePic;
-    } **/
+     public void setGeneratedProfilePic(ProfilePic generatedProfilePic) {
+     this.generatedProfilePic = generatedProfilePic;
+     } **/
 
     public Boolean getIsEntrant() {
         return isEntrant;
@@ -101,6 +102,14 @@ public class User {
 
     public void setIsAdmin(Boolean admin) {
         isAdmin = admin;
+    }
+
+    public Boolean getIsOptedOut() { // Getter for isOptedOut
+        return isOptedOut;
+    }
+
+    public void setIsOptedOut(Boolean isOptedOut) { // Setter for isOptedOut
+        this.isOptedOut = isOptedOut;
     }
 
     // other methods (specific to User) beyond Gs & Ss: can be moved around in package but for now I am placing them here
