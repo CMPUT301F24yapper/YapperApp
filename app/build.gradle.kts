ea9d8ab3add22f40292fd3b208cbade8435c9d78
Mohammedmostain <122962236+Mohammedmostain@users.noreply.github.com>
2024-11-05 16:00:15 -0700

    fixed the gradle files to make sure it works with the notification thing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,17 +34,24 @@ android {
 
 dependencies {
     implementation("com.github.bumptech.glide:glide:4.12.0")
-    // Glide v4 uses this new annotation processor -- see https://bumptech.github.io/glide/doc/generatedapi.html
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
+    // Use Firebase BoM to manage Firebase library versions
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+
+    // Declare Firebase libraries without versions; BoM manages these
     implementation("com.google.firebase:firebase-firestore")
-    implementation ("com.google.android.gms:play-services-base:18.2.0")
+    implementation("com.google.firebase:firebase-messaging")
+
+    implementation("com.google.android.gms:play-services-base:18.2.0")
     implementation("com.google.zxing:core:3.4.1")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
