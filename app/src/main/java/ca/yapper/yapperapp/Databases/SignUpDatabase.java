83a53b6281ae1196b10d2edc46e997c5b3abfcd7
bostonecb <boston1@ualberta.ca>
2024-11-20 10:52:57 -0700

    Moving all signupActivity database connection logic over to SignUpDatabase.java

--- app/src/main/java/ca/yapper/yapperapp/Databases/SignUpDatabase.java
+++ app/src/main/java/ca/yapper/yapperapp/Databases/SignUpDatabase.java
@@ -1,4 +1,51 @@
 package ca.yapper.yapperapp.Databases;
 
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import ca.yapper.yapperapp.Activities.SignupActivity;
+
 public class SignUpDatabase {
+
+    private static final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    /**
+     * Checks if a user exists in the database based on the device ID.
+     */
+    public static Task<Boolean> checkUserExists(String deviceId) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        TaskCompletionSource<Boolean> tcs = new TaskCompletionSource<>();
+
+        db.collection("Users").document(deviceId)
+                .get()
+                .addOnSuccessListener(document -> tcs.setResult(document.exists()))
+                .addOnFailureListener(tcs::setException);
+        return tcs.getTask();
+    }
+
+    /**
+     * Creates a new user in the database with the provided details.
+     */
+    public static Task<Void> createUser(String deviceId, String name, String phone, String email) {
+        Map<String, Object> userMap = new HashMap<>();
+        userMap.put("deviceId", deviceId);
+        userMap.put("entrantName", name);
+        userMap.put("entrantPhone", phone);
+        userMap.put("entrantEmail", email);
+        userMap.put("Admin", false);
+        userMap.put("Entrant", true);
+        userMap.put("Organizer", false);
+        userMap.put("notificationsEnabled", true);
+
+        return db.collection("Users").document(deviceId).set(userMap);
+    }
+
 }
