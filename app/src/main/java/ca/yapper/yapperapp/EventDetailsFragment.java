f232cb01bb1744df6bea3144f942d811bd19fe78
Ekam Rajveer Singh Kumar <ekamrajv@ualberta.ca>
2024-11-21 16:17:18 -0700

    xml & edfrag

--- app/src/main/java/ca/yapper/yapperapp/EventDetailsFragment.java
+++ app/src/main/java/ca/yapper/yapperapp/EventDetailsFragment.java
@@ -123,6 +123,7 @@ public class EventDetailsFragment extends Fragment {
                 facilityLocationTextView.setText("Location: " + event.getFacilityLocation());
                 waitListTextView.setText(String.valueOf(event.getWaitListCapacity()));
                 capacityTextView.setText(String.valueOf(event.getCapacity()));
+                descriptionTextView.setText(event.getDescription());
 
                 geolocationEnabled = event.isGeolocationEnabled();
                 if (geolocationEnabled) {
