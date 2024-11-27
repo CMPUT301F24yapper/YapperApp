a14ab1d4370755313e8b4f67af546f0013e42cc6
Induwara Kandapahala <kandapah@ualberta.ca>
2024-11-26 21:13:03 -0700

    Code Clean UP

--- app/src/main/java/ca/yapper/yapperapp/OrganizerFragments/OrganizerCreateEditEventFragment.java
+++ app/src/main/java/ca/yapper/yapperapp/OrganizerFragments/OrganizerCreateEditEventFragment.java
@@ -36,27 +36,23 @@ import ca.yapper.yapperapp.UMLClasses.Event;
  */
 public class OrganizerCreateEditEventFragment extends Fragment {
 
+//-------------------------------------------UI Components------------------------------------------------
+
+    private TextView dateTextView, timeTextView, regDeadlineTextView;
     private EditText eventNameEditText;
-    private TextView facilityNameTextView;
-    private TextView facilityAddressTextView;
-    private EditText eventDateEditText;
-    private EditText eventTimeEditText;
-    private EditText eventDeadlineEditText;
     private EditText eventCapacityEditText;
     private EditText eventWaitListCapacityEditText;
     private EditText eventDescriptionEditText;
-    private Switch geolocationSwitch;
-    private Button saveEventButton;
     private String userDeviceId;
     private String eventId;
     private String facilityNameFinal;
     private String facilityAddressFinal;
-    private int selectedYear, selectedMonth, selectedDay;
     private String selectedDate, selectedTime, regDeadline;
-    private TextView dateTextView, timeTextView, regDeadlineTextView;
     private Button dateButton, timeButton, regDeadlineButton;
+    private Switch geolocationSwitch;
+    private Button saveEventButton;
+    private int selectedYear, selectedMonth, selectedDay;
 
-    //private boolean exitCreation = false;
 
     /**
      * Inflates the fragment layout, initializes form fields, and sets up date pickers
@@ -73,12 +69,12 @@ public class OrganizerCreateEditEventFragment extends Fragment {
         View view = inflater.inflate(R.layout.organizer_create_edit_event, container, false);
 
         initializeFields(view);
+        setupClickListeners();
 
-        // Check if eventId is passed to edit an event
         Bundle bundle = getArguments();
         if (bundle != null && bundle.containsKey("eventId")) {
             eventId = bundle.getString("eventId");
-            loadEventDetails(eventId); // Load event details if editing
+            loadEventDetails(eventId);
         }
 
         saveEventButton.setOnClickListener(v -> {
@@ -89,13 +85,10 @@ public class OrganizerCreateEditEventFragment extends Fragment {
             }
         });
 
-        setupClickListeners();
-
         userDeviceId = Settings.Secure.getString(requireContext().getContentResolver(), Settings.Secure.ANDROID_ID);
 
         return view;
     }
-
 
     /**
      * Initializes the UI components of the event creation form, setting up EditText fields
@@ -103,15 +96,14 @@ public class OrganizerCreateEditEventFragment extends Fragment {
      *
      * @param view The root view of the fragment layout.
      */
+
+    //---------------------------------------Initialization--------------------------------------------------------
     private void initializeFields(View view) {
         eventNameEditText = view.findViewById(R.id.event_name_input);
         eventDescriptionEditText = view.findViewById(R.id.event_description);
         dateButton = view.findViewById(R.id.date_button);
         timeButton = view.findViewById(R.id.time_button);
         regDeadlineButton = view.findViewById(R.id.reg_deadline_button);
-        // eventDateEditText = view.findViewById(R.id.date_input);
-        // eventTimeEditText = view.findViewById(R.id.time_input);
-        // eventDeadlineEditText = view.findViewById(R.id.deadline_input);
         dateTextView = view.findViewById(R.id.date_textview);
         timeTextView = view.findViewById(R.id.time_textview);
         regDeadlineTextView = view.findViewById(R.id.reg_deadline_textview);
@@ -126,23 +118,14 @@ public class OrganizerCreateEditEventFragment extends Fragment {
      * when clicked, and configures the save button to trigger event creation.
      */
     private void setupClickListeners() {
-        // Date picker
+
         dateButton.setOnClickListener(v -> openDatePicker());
-
-        // Time Picker
         timeButton.setOnClickListener(v -> openTimePicker());
-
-        // Date Picker for Registration Deadline
         regDeadlineButton.setOnClickListener(v -> openRegDeadlinePicker());
 
-        // eventDateEditText.setOnClickListener(v -> showDatePickerDialog(eventDateEditText));
-        // eventTimeEditText.setOnClickListener(v -> showTimePickerDialog(eventTimeEditText));
-        // eventDeadlineEditText.setOnClickListener(v -> showDatePickerDialog(eventDeadlineEditText));
-
-        // save event click listener should be invoked AFTER all the (required) details have been entered in by organizer
-        // saveEventButton.setOnClickListener(v -> saveOrUpdateEvent());
     }
 
+    //------------------------------------Time Picker----------------------------------------------------------
     private void openDatePicker() {
         Calendar calendar = Calendar.getInstance();
         DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
@@ -174,12 +157,10 @@ public class OrganizerCreateEditEventFragment extends Fragment {
                 },
                 calendar.get(Calendar.HOUR_OF_DAY),
                 calendar.get(Calendar.MINUTE),
-                false // Use 12-hour format (AM/PM)
+                false
         );
         timePickerDialog.show();
     }
-
-
 
     private void openRegDeadlinePicker() {
         Calendar calendar = Calendar.getInstance();
@@ -196,142 +177,14 @@ public class OrganizerCreateEditEventFragment extends Fragment {
         datePickerDialog.show();
     }
 
-    /**
-     * Displays a DatePickerDialog, allowing the user to select a date for the event or registration deadline.
-     *
-     * @param dateEditText The EditText field where the selected date will be displayed.
-     */
-    private void showDatePickerDialog(EditText dateEditText) {
-        final Calendar calendar = Calendar.getInstance();
-        selectedYear = calendar.get(Calendar.YEAR);
-        selectedMonth = calendar.get(Calendar.MONTH);
-        selectedDay = calendar.get(Calendar.DAY_OF_MONTH);
-
-        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
-                (view, year, month, dayOfMonth) -> {
-                    String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
-                    dateEditText.setText(selectedDate);
-                }, selectedYear, selectedMonth, selectedDay);
-
-        datePickerDialog.show();
-    }
-
-    private void showTimePickerDialog(EditText timeEditText) {
-        final Calendar calendar = Calendar.getInstance();
-        int selectedHour = calendar.get(Calendar.HOUR_OF_DAY);
-        int selectedMinute = calendar.get(Calendar.MINUTE);
-
-        TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(),
-                (view, hourOfDay, minute) -> {
-                    // Format the time as HH:mm
-                    String selectedTime = String.format("%02d:%02d", hourOfDay, minute);
-                    timeEditText.setText(selectedTime);
-                }, selectedHour, selectedMinute, true);  // true for 24-hour format, false for 12-hour format
-
-        timePickerDialog.show();
-    }
-
-    private void setButtonState(String text, int color) {
-        saveEventButton.setText(text);
-        saveEventButton.setBackgroundColor(color);
-    }
-
-    /**
-     * Validates the input fields and saves the event details to Firestore.
-     * Displays confirmation messages if successful or error messages if fields are missing.
-     */
-    private void createEvent() {
-        // Load facility details from Firestore
-        /** OrganizerDatabase.loadFacilityData(userDeviceId, new OrganizerDatabase.OnFacilityDataLoadedListener() {
-            @Override
-            public void onFacilityDataLoaded(String facilityName, String facilityAddress) {
-                if (facilityName == null || facilityAddress == null || facilityName.isEmpty() || facilityAddress.isEmpty()) {
-                    Toast.makeText(getContext(), "Error: Facility details are required to create an event. Please update this in your profile.", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                String eventName = eventNameEditText.getText().toString();
-                if (eventName.isEmpty()) {
-                    Toast.makeText(getActivity(), "Event name is required", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                /**String dateTime = eventDateEditText.getText().toString();
-                if (dateTime.isEmpty()) {
-                    Toast.makeText(getActivity(), "Event date is required", Toast.LENGTH_SHORT).show();
-                    return;
-                }**/
-                /**
-                String description = eventDescriptionEditText.getText().toString();
-                if (description.isEmpty()) {
-                    Toast.makeText(getActivity(), "Event description is required", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                /**String regDeadline = eventDeadlineEditText.getText().toString();
-                if (regDeadline.isEmpty()) {
-                    Toast.makeText(getActivity(), "Registration deadline is required", Toast.LENGTH_SHORT).show();
-                    return;
-                }**/
-                /**
-                int capacityInt;
-                int waitListCapacityInt;
-
-                String capacityString = eventCapacityEditText.getText().toString();
-                if (capacityString.isEmpty()) {
-                    Toast.makeText(getActivity(), "Number of attendees is required", Toast.LENGTH_SHORT).show();
-                    return;
-                } else {
-                    capacityInt = Integer.parseInt(capacityString);
-                }
-
-                String waitListCapacityString = eventWaitListCapacityEditText.getText().toString();
-                if (waitListCapacityString.isEmpty()) {
-                    waitListCapacityInt = 0;
-                } else {
-                    waitListCapacityInt = Integer.parseInt(waitListCapacityString);
-                }
-
-                boolean geolocationEnabled = geolocationSwitch.isChecked();
-
-                // Proceed with the rest of the event creation logic
-
-                OrganizerDatabase.createEventInDatabase(
-                        capacityInt,
-                        dateTime,
-                        description,
-                        facilityAddress,
-                        facilityName,
-                        geolocationEnabled,
-                        eventName,
-                        regDeadline,
-                        waitListCapacityInt,
-                        userDeviceId
-                );
-
-                // Disable the save button and change color after event creation
-                saveEventButton.setEnabled(false);
-                setButtonState("Event saved", Color.GRAY);
-
-                // Show success message
-                Toast.makeText(getActivity(), "Event created successfully!", Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onError(String error) {
-                Toast.makeText(getContext(), "Error: you have not saved your Facility details. Please update this in your profile.", Toast.LENGTH_LONG).show();
-            }
-        }); **/
-    }
+    //------------------------------------DataBase Operation-------------------------------------------------
 
     private void loadEventDetails(String eventId) {
         OrganizerDatabase.loadEventFromDatabase(eventId, new OrganizerDatabase.OnEventLoadedListener() {
             @Override
             public void onEventLoaded(Event event) {
                 eventNameEditText.setText(event.getName());
-                // eventDateEditText.setText(event.getDate_Time());
                 regDeadlineTextView.setText(event.getRegistrationDeadline());
-                // eventDeadlineEditText.setText(event.getRegistrationDeadline());
                 eventCapacityEditText.setText(String.valueOf(event.getCapacity()));
 
                 if (event.getWaitListCapacity() != null) {
